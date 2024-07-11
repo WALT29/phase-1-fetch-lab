abea0920a254d8c63d1007d0d00b94b57af86a6e
WALTER DAVID IRUNGU <daveirungu20@gmail.com>
2024-07-11 03:20:15 +0300

    HOW TO USE FETCH

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(response=>response.json())
+  .then(data=>renderBooks(data));
 }
 
 function renderBooks(books) {
@@ -11,7 +13,6 @@ function renderBooks(books) {
     main.appendChild(h2);
   });
 }
-
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
